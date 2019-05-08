bc03af4977608837ca7723cc837c3ff3afcdb1a9
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-05-07 22:10:34 -0300

    realizando exercicios

--- src/br/com/livraria/bean/LivroBean.java
+++ src/br/com/livraria/bean/LivroBean.java
@@ -1,17 +1,45 @@
 package br.com.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.livraria.dao.DAO;
+import br.com.livraria.modelo.Autor;
 import br.com.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped //a cada request é gerado um novo livroBean, viewScoped evita isso
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	private Integer autorId;
+	
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+	
+	public Integer getAutorId() {
+		return autorId;
+	}
 
 	public Livro getLivro() {
 		return livro;
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return livro.getAutores();
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(autorId);
+		livro.adicionaAutor(autor);
+		System.out.println("Livro escrito por " + autor.getNome());
 	}
 
 	public void gravar() {
