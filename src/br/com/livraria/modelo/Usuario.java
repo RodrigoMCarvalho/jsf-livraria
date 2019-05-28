107af2a0233d807ce24915c86b3ce695ad923549
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2019-05-27 22:49:45 -0300

    autenticacao do usuario

---
+++ src/br/com/livraria/modelo/Usuario.java
@@ -0,0 +1,41 @@
+package br.com.livraria.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String email;
+	private String senha;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
